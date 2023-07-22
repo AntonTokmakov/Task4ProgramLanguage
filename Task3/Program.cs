0514c850e957a8dca45c926d206335eb7d95df3a
Anton <antontokmakov757@gmail.com>
2023-07-22 13:52:26 +0700

    func complited tasks

--- Task3/Program.cs
+++ Task3/Program.cs
@@ -1,11 +1,26 @@
 ﻿Console.WriteLine("Введите массив");
-List<int> array = new List<int>();
+List<int> arr = InList();
+PrintArray(arr);
 
-while (true)
+List<int> InList()
 {
-    string num = Console.ReadLine();
-    if (num.Equals("Stop") || num.Equals("stop")) break;
-    array.Add(int.Parse(num));
+    int count = 0;
+    List<int> array = new List<int>();
+    while (count != 8)
+    {
+        string num = Console.ReadLine();
+        if (num.Equals("Stop") || num.Equals("stop")) break;
+        array.Add(int.Parse(num));
+        count++;
+    }   
+    return array; 
 }
 
-Console.WriteLine(string.Join(", ", array));
+void PrintArray(List<int> array)
+{
+    Console.WriteLine("Вывод массива");
+    for (int i = 0; i < array.Count; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+}
